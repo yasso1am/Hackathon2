81f59691299862b212e18e5453afe2fa0b61ad36
Andrew Yasso <andrew.yasso@gmail.com>
2018-06-30 13:53:18 -0600

    fixezd loader

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -11,25 +11,25 @@ import { Link } from 'react-router-dom';
 
 class NoMatch extends Component {
 
-
-
-  componentWillMount(){
-    setTimeout(() => { 
-        this.props.history.push('/');
-    }, 4000)
-}
+//   componentWillMount(){
+//     setTimeout(() => { 
+//         this.props.history.push('/');
+//     }, 4000)
+// }
   render() {
     return (
-      <Container>
-        <Dimmer active inverted>
-          <Loader inverted>
-            <Header as='h1' textAlign='center'>
-                Page Not Found - Redirecting To Home Page
-            </Header>
-            <Link to='/'> Home </Link>
-          </Loader>
-        </Dimmer>
-      </Container>
+      <div>
+      </div>
+      // <Container>
+      //   <Dimmer active inverted>
+      //     <Loader inverted>
+      //       <Header as='h1' textAlign='center'>
+      //           Page Not Found - Redirecting To Home Page
+      //       </Header>
+      //       <Link to='/'> Home </Link>
+      //     </Loader>
+      //   </Dimmer>
+      // </Container>
     );
   }
 }
