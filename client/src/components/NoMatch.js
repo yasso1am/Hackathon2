24d378f53a9935d8facad2cd11a317e2cd81a29a
Andrew Yasso <andrew.yasso@gmail.com>
2018-06-30 13:04:42 -0600

    404 updated

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { 
+  Container,
+  Header,
+  Dimmer,
+  Loader,
+} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+
+
 class NoMatch extends Component {
+
+
+
+  componentWillMount(){
+    setTimeout(() => { 
+        this.props.history.push('/');
+    }, 4000)
+}
   render() {
     return (
-      <Header as='h1' textAlign='center'>
-        Page Not Found
-        <Link to='/'> Home</Link>
-      </Header>
+      <Container>
+        <Dimmer active inverted>
+          <Loader inverted>
+            <Header as='h1' textAlign='center'>
+                Page Not Found - Redirecting To Home Page
+            </Header>
+            <Link to='/'> Home </Link>
+          </Loader>
+        </Dimmer>
+      </Container>
     );
   }
 }
